640bb6b6f10ebc5c05d59ed3184b8af947cfa016
sid <sid19arya@gmail.com>
2025-02-28 17:43:36 -0500

    added 2 joint sampling to for full info

--- bayesian_dpddm/monitors/utils.py
+++ bayesian_dpddm/monitors/utils.py
@@ -28,6 +28,40 @@ s
         true_labels[i] = dataset[indices[i]][1]
     return tmp, true_labels
 
+def joint_sample_from_datasets(n_samples: int, datasetA:Dataset, datasetB: Dataset, balance_ratio:float = 1, replace=True):
+    """
+    Given two datasets, sample (n * balance_ratio) samples from A, and (n * (1-balance_ratio)) samples from B
+    Args:
+        n_samples (int): number of samples to sample
+        datasetA (Dataset): torch Dataset object
+        datasetB (Dataset): torch Dataset object
+        balance_ratio (float): ratio of samples to sample from datasetA
+        replace (bool, optional): Whether to sample with replacement. Defaults to True.
+clear
+s
+    Returns:
+        torch.tensor: sample from dataset.
+        torch.tensor: mask of the samples, 1 if sample is from datasetA, 0 if sample is from datasetB
+    """
+    
+    assert 0 <= balance_ratio <= 1, "balance_ratio must be between 0 and 1"
+    n_samples_A = int(n_samples * balance_ratio)
+    n_samples_B = n_samples - n_samples_A
+    data_size = datasetA[0][0].size()
+    indices_A = np.random.choice(np.arange(len(datasetA)), n_samples_A, replace=replace)
+    indices_B = np.random.choice(np.arange(len(datasetB)), n_samples_B, replace=replace)
+    tmp = torch.zeros(size=(n_samples, *data_size))
+    true_labels = torch.zeros(size=(n_samples,))
+    mask = torch.zeros(size=(n_samples,))
+    for i in range(len(indices_A)):
+        tmp[i] = datasetA[indices_A[i]][0]
+        true_labels[i] = datasetA[indices_A[i]][1]
+        mask[i] = 1
+    for i in range(len(indices_B)):
+        tmp[i + n_samples_A] = datasetB[indices_B[i]][0]
+        true_labels[i + n_samples_A] = datasetB[indices_B[i]][1]
+    return tmp, true_labels, mask
+
 
 def get_class_from_string(class_string):
     """Given a class name, return the module to be instantiated.
