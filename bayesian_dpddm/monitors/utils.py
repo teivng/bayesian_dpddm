9d5cfe3cbcfb59c46163f201e49f8ff851e5bd38
sid <sid19arya@gmail.com>
2025-02-28 17:48:47 -0500

    custom loss function for full info moniter

--- bayesian_dpddm/monitors/utils.py
+++ bayesian_dpddm/monitors/utils.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 import numpy as np
 import torch
+import torch.nn.functional as F
+from typing import Optional
 
 
 def temperature_scaling(logits, temperature):
@@ -83,3 +85,47 @@ def get_class_from_string(class_string):
     cls = getattr(module, class_name)
     
     return cls
+
+
+class FILoss(torch.nn.Module):
+    def __init__(self, weight=None, alpha=None):
+        super(FILoss, self).__init__()
+        self.weight = weight
+        self.alpha = alpha
+
+    def forward(self, logits, labels, mask):
+        return self.full_info_loss(logits, labels,
+                        weight=self.weight,
+                        mask=mask,
+                        alpha=self.alpha)
+    
+    def full_info_loss(logits: torch.Tensor, labels: torch.Tensor, mask: torch.Tensor, alpha: Optional[float] = None,
+             weight=None) -> torch.Tensor:
+        """
+        :param logits: (batch_size, num_classes) tensor of logits
+        :param labels: (batch_size,) tensor of labels
+        :param mask: (batch_size,) mask
+        :param alpha: (float) weight of datasetB samples
+        :param weight:  (torch.Tensor) weight for each sample_data, default=None do not apply weighting
+        :return: (tensor, float) the disagreement cross entropy loss
+        """
+        if mask.all():
+            # if all labels are positive, then use the standard cross entropy loss
+            return F.cross_entropy(logits, labels)
+
+        num_classes = logits.shape[1]
+
+        q_logits, q_labels = logits[~mask], labels[~mask]
+
+        zero_hot = 1. - F.one_hot(q_labels, num_classes=num_classes)
+        ce_n = -(q_logits * zero_hot).sum(dim=1) / (num_classes - 1) + torch.logsumexp(q_logits, dim=1)
+
+        if torch.isinf(ce_n).any() or torch.isnan(ce_n).any():
+            raise RuntimeError('NaN or Infinite loss encountered for ce-q')
+
+        if (~mask).all():
+            return (ce_n * alpha).mean()
+
+        p_logits, p_labels = logits[mask], labels[mask]
+        ce_p = F.cross_entropy(p_logits, p_labels, reduction='none', weight=weight)
+        return torch.cat([ce_n * alpha, ce_p]).mean()
