5d285bb851d71d4216755a3e6b194505fb836db6
teivng <viet@cs.toronto.edu>
2025-04-09 12:40:28 -0400

    message

--- bayesian_dpddm/__init__.py
+++ bayesian_dpddm/__init__.py
@@ -1,6 +1,6 @@
 from .models import ConvModel, MLPModel
 from .monitors import DPDDMBayesianMonitor, DPDDMFullInformationMonitor
-from ._metadata import *
+from ._metadata import __author__, __description__, __email__, __license__, __url__
 from importlib.metadata import version, PackageNotFoundError
 
 try:
