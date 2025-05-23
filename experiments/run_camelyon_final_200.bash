c8565d6082b6065381b1c2f296c207902fff1e2b
teivng <viet@cs.toronto.edu>
2025-05-23 16:17:09 +0000

    generic message

---
+++ experiments/run_camelyon_final_200.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONFIG_NAME="camelyon17_best"
+SAMPLE_SIZE=200
+
+for SEED in $(seq 67 76); do
+    echo "Running $CONFIG_NAME with seed=$SEED and sample_size=$SAMPLE_SIZE"
+    python experiments/run.py --config-name=$CONFIG_NAME seed=$SEED dpddm.data_sample_size=$SAMPLE_SIZE
+done
---
+++ experiments/run_cifar_final_100.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONFIG_NAME="cifar10_best_"
+SAMPLE_SIZE=100
+
+for SEED in $(seq 57 76); do
+    echo "Running $CONFIG_NAME with seed=$SEED and sample_size=$SAMPLE_SIZE"
+    python experiments/run.py --config-name=$CONFIG_NAME seed=$SEED dpddm.data_sample_size=$SAMPLE_SIZE
+done
